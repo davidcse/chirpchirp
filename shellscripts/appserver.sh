b31a5b5fa38a04e99728e2f70e44d0e202969c3b
Elvis Fernandez <eaf207@gmail.com>
2017-04-15 20:42:52 -0400

    more changes to appserver script

--- shellscripts/appserver.sh
+++ shellscripts/appserver.sh
@@ -14,20 +14,30 @@ echo $seperator "Installing Git" $seperator
 sudo apt-get update
 sudo apt-get install git
 
-
 # clone repo
 echo $seperator "Cloning Project" $seperator
 git clone $project_repo $project_dest
 
+# install all project dependencies (python related)
+echo $seperator "Update apt-get" $seperator
+sudo apt-get update
+echo $seperator "Installing python-dev" $seperator
+sudo apt-get install python2.7-dev
+echo $seperator "Installing python-pip" $seperator
+sudo apt-get install python-pip
+echo $seperator "Installing uwsgi" $seperator
+sudo pip install uwsgi
+
 # install nginx
-echo $seperator "Updating apt-get pm" $seperator
+echo $seperator "Updating apt-get" $seperator
 sudo apt-get update
 echo $seperator "Installing Nginx" $seperator
 sudo apt-get install nginx
 
 # confingure nginx
 #sudo uwsgi --http :80 --wsgi-file /home/ubuntu/chirpchirp/tweet/tweet/wsgi.py --master --processes 4 --threads 1
-sudo ln -s /home/ubuntu/chirpchirp/chirp_nginx.conf /etc/nginx/sites-enabled/
+uwsgi --socket chirp.sock --wsgi-file /home/ubuntu/chirpchirp/tweet/tweet/wsgi.py --master --processes 1 --threads 1
+sudo ln -s /home/ubuntu/chirpchirp/nginxconfig/chirp_nginx.conf /etc/nginx/sites-enabled/
 
 
 # run nginx

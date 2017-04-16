c2e7fd71189fd86a79739303c76f34ead0c60b5b
Elvis Fernandez <eaf207@gmail.com>
2017-04-15 23:57:24 -0400

    more changes to appserver script

--- shellscripts/appserver.sh
+++ shellscripts/appserver.sh
@@ -9,26 +9,25 @@ seperator="---------------------"
 
 # install git
 echo $seperator "Installing Git" $seperator
+sleep 2
 sudo apt-get update
-sleep 2
 sudo apt-get install git
-sleep 2
 
 # clone repo
 echo $seperator "Cloning Project" $seperator
-git clone $project_repo
 sleep 2
+git clone $project_repo
 
 # install all project dependencies (python related)
 echo $seperator "Update apt-get" $seperator
+sleep 2
 sudo apt-get update
-sleep 2
 echo $seperator "Installing python-dev" $seperator
+sleep 2
 sudo apt-get install python2.7-dev
-sleep 2
 echo $seperator "Installing python-pip" $seperator
-sudo apt-get install python-pip
 sleep 2
+sudo apt-get install python-pip
 
 # activate virtualenv
 echo $seperator "Activate virtual-env" $seperator
@@ -36,40 +35,43 @@ cd chirpchirp
 source bin/activate
 echo $seperator "Moving to /chirpchirp/tweet dir" $seperator
 cd tweet
+echo $seperator "Collecting static" $seperator
+python manage.py collectstatic
+
 echo $seperator "Installing uwsgi" $seperator
+sleep 2
 sudo pip install uwsgi
-sleep 2
 echo $seperator "Installing django" $seperator
+sleep 2
 sudo pip install django
-sleep 2
 echo $seperator "Installing pymongo" $seperator
+sleep 2
 sudo pip install pymongo
-sleep 2
 echo $seperator "Installing memcached" $seperator
-sudo pip install python-memcached
 sleep 2
+sudo pip install python-memcached
 
 # install nginx
 echo $seperator "Updating apt-get" $seperator
+sleep 2
 sudo apt-get update
-sleep 2
 echo $seperator "Installing Nginx" $seperator
-sudo apt-get install nginx
 sleep 2
+sudo apt-get install nginx
 
 # confingure nginx
 echo $seperator "Launching uwsgi" $seperator
+sleep 2
 #sudo uwsgi --socket /home/ubuntu/chirp.sock --wsgi-file /home/ubuntu/chirpchirp/tweet/tweet/wsgi.py --master --processes 10 --threads 2 --chmod-socket=666 --logto /home/ubuntu/uwsgi.log --daemonize /home/ubuntu/daemonize.log
 sudo uwsgi --socket /home/ubuntu/chirp.sock --wsgi-file /home/ubuntu/chirpchirp/tweet/tweet/wsgi.py --master --processes 1 --threads 1 --chmod-socket=666 --logto /home/ubuntu/uwsgi.log --daemonize /home/ubuntu/daemonize.log
-sleep 2
 echo $seperator "Connecting nginx to uwsgi" $seperator
-sudo ln -s /home/ubuntu/chirpchirp/nginxconfig/chirp_nginx.conf /etc/nginx/sites-enabled/
 sleep 2
+sudo ln -s /home/ubuntu/chirpchirp/nginxconfig/chirp_nginx.conf /etc/nginx/sites-enabled/
 
 # run nginx
 echo $seperator "Restarting nginx" $seperator
+sleep 2
 sudo service nginx restart
-sleep 2
 echo $seperator "Nginx Status" $seperator
-sudo service nginx status
 sleep 2
+sudo service nginx status

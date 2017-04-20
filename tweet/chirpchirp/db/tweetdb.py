e52d6c6809360e2a322ea4ec15eee581e1b55542
Elvis Fernandez <eaf207@gmail.com>
2017-04-20 11:00:05 -0400

    small bug yet again

--- tweet/chirpchirp/db/tweetdb.py
+++ tweet/chirpchirp/db/tweetdb.py
@@ -18,7 +18,7 @@ class TweetDB:
         self.like = like
         # connect to mongo
         # self.client = MongoClient('127.0.0.1', 27017)
-        self.client = MongoClient('130.245.169.41', 27017)
+        self.client = MongoClient('192.168.1.54', 27017)
         self.db = self.client.tweet
         self.userDB = self.db.user
         self.tweetsDB = self.db.tweets

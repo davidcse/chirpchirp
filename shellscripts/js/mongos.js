708e355dafe1eacbe90432b61ef8595e5c9dad0a
Elvis Fernandez <eaf207@gmail.com>
2017-04-20 10:59:38 -0400

    small bug yet again

--- shellscripts/js/mongos.js
+++ shellscripts/js/mongos.js
@@ -1,9 +1,9 @@
 //verifying this is committed
 use tweet;
-sh.addShard("shard1_ip:shard1_port");
-sh.addShard("shard2_ip:shard2_port");
-sh.addShard("shard3_ip:shard3_port");
-sh.addShard("shard4_ip:shard4_port");
+sh.addShard("shard1/shard1_ip:shard1_port");
+sh.addShard("shard1/shard2_ip:shard2_port");
+sh.addShard("shard1/shard3_ip:shard3_port");
+sh.addShard("shard1/shard4_ip:shard4_port");
 sh.enableSharding("tweet");
 sh.shardCollection("tweet.user", {"username": 1});
 sh.shardCollection("tweet.tweets", {"username": 1})

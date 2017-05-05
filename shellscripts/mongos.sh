f6ff11408ee29ea21537f9b2aaef123c1e52c57a
Elvis Fernandez <eaf207@gmail.com>
2017-05-05 13:14:54 -0400

    small edits setting up for deployment

--- shellscripts/mongos.sh
+++ shellscripts/mongos.sh
@@ -4,19 +4,24 @@
 # USAGE: ./mongos.sh {host_ip} {config_ip} {config_port} {shard1_ip} {27040} {shard2_ip} {shard2_port} {shard3_ip} {shard3_port} {shard4_ip} {shard4_port}
 # USAGE: sudo ./mongos.sh 192.168.1.54 192.168.1.49 27030 192.168.1.45 27040 192.168.1.46 27040 192.168.1.47 27040 192.168.1.48 27040
 
+
+# USAGE: ./mongos.sh {host_ip} {config_ip} {config_port} {shard1_ip} {shard2_ip} {shard3_ip} {shard4_ip} {shard5_ip} {shard6_ip} {shard7_ip} {shard8_ip}
+# USAGE: ./mongos.sh {54.175.131.133} {172.31.31.208} {27030} {172.31.20.176} {172.31.18.41} {172.31.21.197} {172.31.17.160} {172.31.28.21} {172.31.16.102} {172.31.16.97} {172.31.20.129}
+
+
 # script variables
 seperator="---------------------"
 host_ip=${1}
 config_ip=${2}
 config_port=${3}
 shard1_ip=${4}
-shard1_port=${5}
-shard2_ip=${6}
-shard2_port=${7}
-shard3_ip=${8}
-shard3_port=${9}
-shard4_ip=${10}
-shard4_port=${11}
+shard2_ip=${5}
+shard3_ip=${6}
+shard4_ip=${7}
+shard5_ip=${8}
+shard6_ip=${9}
+shard7_ip=${10}
+shard8_ip=${11}
 sleep_limit=5
 mongos_js="https://raw.githubusercontent.com/elvis-alexander/chirpchirp/master/shellscripts/js/mongos.js"
 
@@ -51,13 +56,14 @@ wget $mongos_js
 echo $seperator "Setting up config file..." $seperator
 sleep 2
 sed -ie 's/shard1_ip/'$shard1_ip'/g' mongos.js
-sed -ie 's/shard1_port/'$shard1_port'/g' mongos.js
 sed -ie 's/shard2_ip/'$shard2_ip'/g' mongos.js
-sed -ie 's/shard2_port/'$shard2_port'/g' mongos.js
 sed -ie 's/shard3_ip/'$shard3_ip'/g' mongos.js
-sed -ie 's/shard3_port/'$shard3_port'/g' mongos.js
 sed -ie 's/shard4_ip/'$shard4_ip'/g' mongos.js
-sed -ie 's/shard4_port/'$shard4_port'/g' mongos.js
+sed -ie 's/shard5_ip/'$shard5_ip'/g' mongos.js
+sed -ie 's/shard6_ip/'$shard6_ip'/g' mongos.js
+sed -ie 's/shard7_ip/'$shard7_ip'/g' mongos.js
+sed -ie 's/shard8_ip/'$shard8_ip'/g' mongos.js
+#sed -ie 's/shard1_port/'$shard1_port'/g' mongos.js
 
 # connect mongo client
 echo $seperator "Feeding js input" $seperator
